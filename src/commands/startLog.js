e763686da2187005075771d3f8592d6fbbcb2a50
Octavio Martinez <omartinez1618@gmail.com>
2022-07-30 23:31:46 -0400

    condicion save-workspace

--- src/commands/startLog.js
+++ src/commands/startLog.js
@@ -6,15 +6,19 @@ const startLog = function(context){
     vscode.commands.registerCommand('codinglog.startLog', async function () {
     // The code you place here will be executed every time your command is executed
 
-    // Display a message box to the user
-    vscode.workspace.onDidChangeTextDocument(async (event)=>{
-        //const { fileName, uri } = event.document;
-        const reason = event.reason;
-        const text = event.contentChanges[0].text;
-        keysCount(context.workspaceState, reason, text);
-        
-    })
-    vscode.window.showInformationMessage('Codinglog is now active!');
+    if(context.workspaceState._id.substr(0,9) !== "undefined") {
+        // Display a message box to the user
+        vscode.workspace.onDidChangeTextDocument(async (event)=>{
+            //const { fileName, uri } = event.document;
+            const reason = event.reason;
+            const text = event.contentChanges[0].text;
+            keysCount(context.workspaceState, reason, text);
+        })
+        vscode.window.showInformationMessage('Codinglog is now active!');
+    }else{        
+        vscode.window.showErrorMessage("Save Workspace before")        
+        //vscode.window.showInformationMessage('Codinglog is not active!');
+    }    
     })
 };
 
